e20872f22cbe2941e876c786177a823dc4bac4e5
Ilya Pugachev <ilyapugachv@gmail.com>
2023-12-15 11:18:11 +0300

    Project created

---
+++ TimerApp/TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Илья Пугачёв on 15.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
